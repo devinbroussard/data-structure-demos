f8725a4886a96a0b5a173431f86d83e188c29bdd
Devin <devin.ray.broussard@gmail.com>
2022-02-14 09:58:56 -0600

    Did some linked list testing

--- LinkedLists/LinkedLists/LinkedLists.cpp
+++ LinkedLists/LinkedLists/LinkedLists.cpp
@@ -2,27 +2,4 @@
 #include "List.h"
 
 int main() {
-	List<int> list; 
-	list.addToFront(2);
-	list.addToFront(42);
-	list.addToFront(3);
-	list.addToFront(3);
-	list.addToFront(4223);
-	list.sort();
-	list.print();
-
-	list.remove(3);
-	list.print();
-
-	List<int> list2 = list;
-	//List<int> list3 = List<int>(list);
-
-	list2.print();
-	//list3.print();
-
-	list.clearList();
-	list.print();
-
-	//list3.remove(42);
-
 }
