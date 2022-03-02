ffce202ea35163663719297140c42fbba3d1b240
Devin Broussard <devin.ray.broussard@gmail.com>
2022-03-02 10:42:14 -0600

    Added a short demo of the functionality of the dictionary

--- Projects/Dictionary/main.cpp
+++ Projects/Dictionary/main.cpp
@@ -2,11 +2,58 @@
 #include <iostream>
 
 int main() {
-	Dictionary<char, int> dictionary = Dictionary<char, int>();
+	Dictionary<int, char> dictionary = Dictionary<int, char>();
+	int keys[10]; //An array to store keys for printing key/value pairs
 
-	dictionary.addItem('a', 1);
-	dictionary.remove('2');
-	dictionary.remove('a');
+	//Adding items to dictionary
+	dictionary.addItem(1, 'a');
+	keys[0] = 1;
+	dictionary.addItem(2, 'b');
+	keys[1] = 2;
+	dictionary.addItem(3, 'c');
+	keys[2] = 3;
+	dictionary.addItem(4, 'd');
+	keys[3] = 4;
+	dictionary.addItem(5, 'f');
+	keys[4] = 5;
 
-	dictionary.getCount();
+	//Some examples of the functionality of the dictionary:
+
+
+	std::cout << "Printing keys and values of dictionary:\n";
+
+	for (int i = 0; i < dictionary.getCount(); i++) {
+		char value;
+		dictionary.tryGetValue(keys[i], value);
+
+		std::cout << "Key: " << keys[i];
+		std::cout << " Value: " << value << std::endl;
+	}
+
+	std::cout << "\nRemoving a key/value pair:\n";
+
+	dictionary.remove(3);
+	keys[2] = 4;
+	keys[3] = 5;
+	keys[4] = NULL;
+
+	for (int i = 0; i < dictionary.getCount(); i++) {
+		char value;
+		dictionary.tryGetValue(keys[i], value);
+
+		std::cout << "Key: " << keys[i];
+		std::cout << " Value: " << value << std::endl;
+	}
+
+	std::cout << "\nClearing a dictionary:\n";
+
+	dictionary.clear();
+
+	for (int i = 0; i < dictionary.getCount(); i++) {
+		char value;
+		dictionary.tryGetValue(keys[i], value);
+
+		std::cout << "Key: " << keys[i];
+		std::cout << " Value: " << value << std::endl;
+	}
 }
